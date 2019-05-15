57a92e47a557c4bd21fb834450b8f16c83800d93
Michael Pitts <michaelpitts@Michaels-MacBook-Pro.local>
2019-05-14 22:01:53 -0400

    creating variables file

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "digitalocean_token" {
+  description = "authentication token for digitalocean"
+  type        = "string"
+}
+
+variable "access_id" {
+  description = "access ID for the digitalocean bucket"
+  type        = "string"
+}
+
+variable "secret_key" {
+  description = "secret access key for the digitalocean bucket"
+}
